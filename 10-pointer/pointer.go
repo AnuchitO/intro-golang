43e93a064facab1be26851a2f6b1f20f9df65efb
Anuchit Prasertsang <anuchit.prasertsang@gmail.com>
2021-05-10 11:24:33 +0700

    Update pointer.go

--- 10-pointer/pointer.go
+++ 10-pointer/pointer.go
@@ -6,10 +6,16 @@ func main() {
 	me := "Gopher"
 	fmt.Printf("You are %s\n", me)
 
+	// var addr *string = &me
 	addr := &me
 	fmt.Println(addr)
 	fmt.Printf("%T\n", addr)
 
+	// อ่านค่าที่ addr ชึ้อยู่
+	v := *addr
+	fmt.Println(v)
+
+	// เป็นค่าที่ addr ชึ้อยู่
 	*addr = "Penguin"
 	fmt.Printf("You are %s\n", me)
 }
